e8321dd83d7f45b5bba710d8cb52c6310e056dbe
MartiCaballero <141785085+MartiCaballero@users.noreply.github.com>
2025-07-05 16:18:33 -0300

    feat/ totem services + totem login

--- Front-React/src/Views/LoginTotem/LoginTotem.jsx
+++ Front-React/src/Views/LoginTotem/LoginTotem.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { loginTotem } from "../../Services/loginServices";
 import '../../Styles/LoginTotem.css'; 
 
 function LoginTotem() {
     const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
+    const [usuario, setUsuario] = useState('');
+    const [contraseña, setContraseña] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        navigate('/Welcome'); 
+        setError(''); 
+
+        try{
+            const data = await loginTotem(usuario, contraseña);
+            console.log("Login exitoso. ID Circuito:", data.id_circuito);
+
+            navigate('/Welcome'); 
+        } catch (error) {
+            setError('Usuario o contraseña incorrectos. Intente nuevamente.');
+        } 
     };
 
     return (
@@ -21,9 +36,22 @@ function LoginTotem() {
             <div className="form-container">
                 <form onSubmit={handleSubmit}>
                     <label className="input-label">Usuario</label>
-                    <input type="text" placeholder="totem-circuito-xxx" required />
+                    <input 
+                        type="text" 
+                        placeholder="totem-circuito-xxx" 
+                        value={usuario}
+                        onChange={(e) => setUsuario(e.target.value)}
+                        required 
+                    />
                     <label className="input-label">Contraseña</label>
-                    <input type="password" placeholder="C0ntr4señA" required />
+                    <input 
+                        type="password" 
+                        placeholder="C0ntr4señA"
+                        value={contraseña}
+                        onChange={(e) => setContraseña(e.target.value)} 
+                        required 
+                    />
+                    {error && <p className="error-message">{error}</p>}
                     <button type="submit" className="vote-button">Ingresar</button>
                 </form>
             </div>
